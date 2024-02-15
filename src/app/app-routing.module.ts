e72d6a9cb435b1bf386a1dfe1a5a572e886d1c72
Rohman <rohmanpendo@gmail.com>
2024-02-15 09:50:17 +0700

    chore: adjust config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [{
-  path: 'auth',
-  loadChildren: () =>
-    import('./modules/auth/auth.module').then((m) => m.AuthModule),
-}, {
+const routes: Routes = [
+  {
   path: '',
-  loadChildren: () =>
-    import('./layout/layout.module').then((m) => m.LayoutModule),
-},];
+    component: AppComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './modules/auth/login/login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import {ReactiveFormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
 
   ],
   imports: [
